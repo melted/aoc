68fec4d5782f8c7ca19351d946b6139840abf791
Niklas Larsson <niklas@mm.st>
2015-12-05 06:44:37 +0100

    Do day 5

---
+++ day5.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+bool vowels(string &s) {
+    int count = count_if(s.begin(), s.end(), [](char c) {
+        return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
+    });
+    return count > 2;
+}
+
+bool duped(string &s) {
+    for (int i = 1; i < s.length(); i++) {
+        if (s[i - 1] == s[i]) {
+            return true;
+        }
+    }
+    return false;
+}
+
+bool clear(string &s) {
+    return s.find("ab") == string::npos &&
+           s.find("cd") == string::npos &&
+           s.find("pq") == string::npos &&
+           s.find("xy") == string::npos;
+}
+
+bool two_repeat(string &s) {
+    for (int i = 1; i < s.length(); i++) {
+        string t = s.substr(i + 1);
+        string ss = s.substr(i - 1, 2);
+        if (t.find(ss) != string::npos) {
+            return true;
+        }
+    }
+    return false;
+}
+
+
+bool hole_pair(string &s) {
+    for (int i = 2; i < s.length(); i++) {
+        if (s[i - 2] == s[i]) {
+            return true;
+        }
+    }
+    return false;
+}
+
+int main() {
+    ifstream f("input5.txt");
+    int nice = 0;
+    while (!f.eof()) {
+        string s;
+        f >> s;
+        if (two_repeat(s) && hole_pair(s)) {
+            nice++;
+        }
+    }
+    cout << nice << endl;
+    return 0;
+}
